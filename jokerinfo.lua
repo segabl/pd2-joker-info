d5d828e51ced6c8dc423b041b60e8790505ecf2f
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-12-04 01:45:12 +0100

    Switch to HopLib hooks

--- jokerinfo.lua
+++ jokerinfo.lua
@@ -16,39 +16,20 @@ if not JokerInfo then
     return { nickname = function () return "an unknown force" end }
   end
   
-  Hooks:Add("NetworkReceivedData", "NetworkReceivedDataJokerInfo", function(sender, id, data)
-    local peer = LuaNetworking:GetPeers()[sender]
-    if id == "joker_info" then
-      managers.chat:_receive_message(1, "Joker Info", tostring(data), tweak_data.system_chat_color)
+  Hooks:Add("HopLibOnUnitDied", "HopLibOnUnitDiedJokerInfo", function (unit, damage_info)
+    if managers.groupai:state():is_enemy_converted_to_criminal(unit) then
+      local attacker_info = HopLib.unit_info_manager:get_user_info(damage_info.attacker_unit)
+      local info = HopLib.unit_info_manager:get_info(unit)
+      local text = ""
+      for i, v in ipairs(JokerInfo.messages.death) do
+        if info._kills <= v.threshold or i == #JokerInfo.messages.death then
+          text = table.random(v.texts):gsub("<N>", info:nickname()):gsub("<K>", tostring(info._kills)):gsub("<A>", attacker_info and attacker_info:nickname() or "an unknown force")
+          break
+        end
+      end
+      managers.chat:_receive_message(1, "Joker Info", text, tweak_data.system_chat_color)
     end
   end)
-  
-end
-
-if RequiredScript == "lib/units/enemies/cop/copdamage" then
-
-  local die_original = CopDamage.die
-  function CopDamage:die(attack_data, ...)
-    local result = die_original(self, attack_data, ...)
-    
-    if Network:is_server() then
-      local attacker_info = HopLib.unit_info_manager:get_user_info(attack_data.attacker_unit)
-      if managers.groupai:state():is_enemy_converted_to_criminal(self._unit) then
-        local info = HopLib.unit_info_manager:get_info(self._unit)
-        local text = ""
-        for i, v in ipairs(JokerInfo.messages.death) do
-          if info._kills <= v.threshold or i == #JokerInfo.messages.death then
-            text = table.random(v.texts):gsub("<N>", info:nickname()):gsub("<K>", tostring(info._kills)):gsub("<A>", attacker_info and attacker_info:nickname() or "an unknown force")
-            break
-          end
-        end
-        managers.chat:_receive_message(1, "Joker Info", text, tweak_data.system_chat_color)
-        LuaNetworking:SendToPeers("joker_info", text)
-      end
-    end
-    
-    return result
-  end
   
 end
 
@@ -70,7 +51,6 @@ if RequiredScript == "lib/states/missionendstate" then
       end
       if text ~= "" then
         managers.chat:_receive_message(1, "Joker Info", text, tweak_data.system_chat_color)
-        LuaNetworking:SendToPeers("joker_info", text)
       end
     end)
     return at_enter_original(self, ...)
